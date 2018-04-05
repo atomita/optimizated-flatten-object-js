5e02de8e6848984cad288ccc38bc3fa81784807c
atomita <sleeping.cait.sith+gh@gmail.com>
2018-04-05 14:49:02 +0900

    fix: Output name

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ export default {
   output: {
     file: 'dist/flatten-object.js',
     format: 'umd',
-    name: 'flattenObject',
+    name: 'optimizatedFlattenObject',
     sourceMap: true,
   },
   plugins: [typescript()],
